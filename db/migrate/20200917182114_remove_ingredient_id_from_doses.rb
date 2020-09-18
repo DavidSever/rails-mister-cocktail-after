9fb36cdf1c8fa9e87be729777128b4e99fa7b603
DavidSever <david.sever.ds@gmail.com>
2020-09-18 20:03:34 +0200

    hopfully fixed db

--- db/migrate/20200917182114_remove_ingredient_id_from_doses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIngredientIdFromDoses < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :doses, :ingredient_id, :integer
-  end
-end
--- db/migrate/20200917184407_add_cocktail_id_to_doses.rb
+++
@@ -1,5 +0,0 @@
-class AddCocktailIdToDoses < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :doses, :cocktail, null: false, foreign_key: true
-  end
-end
